50eb75251d73951fe0b9849056a3732220db9611
John Gesimondo <john@jmondo.com>
2013-01-27 20:54:44 -0800

    do it for the cal token user not the current user

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,6 +1,8 @@
 class CalendarsController < ApplicationController
   def show
-    eb_client = EventbriteClient.new({ :access_token => current_user.token })
+    user = User.find_by_cal_token(params[:cal_token])
+    return unless user
+    eb_client = EventbriteClient.new({ :access_token => user.token })
     tickets = eb_client.user_list_tickets({ :type => "all" }) rescue []
     render :layout => false,
       :text => TicketsResponse.new(tickets.parsed_response).tickets.ical_events,
